2ed84d335b72cc92365a2240d9e37ac5c38b5e55
alejandroDpAL <alejandropasaje799@gmail.com>
2023-05-17 16:46:36 -0500

    add fuentes

--- js/media.js
+++ js/media.js
@@ -1,1 +1,13 @@
-alert("hola,estoy conectado")
+let hamburgueMenu = document.getElementById("hamburguerMenu");
+let mainMenu = document.getElementById("mainMenu");
+
+hamburgueMenu.addEventListener("click", function() {
+    if (mainMenu.classList.contains("main-menu")) {
+    mainMenu.classList.add("main-menu-block")
+    mainMenu.classList.remove("main-menu") 
+}
+else {
+    mainMenu.classList.remove("main-menu-block")
+    mainMenu.classList.add("main-menu") 
+}
+});
